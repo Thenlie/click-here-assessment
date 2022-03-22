7a72958f8def424fda18e1f8e366cd678b0f4943
Leithen <leithen113@gmail.com>
2022-03-22 14:04:11 -0600

    Built out test suite with failing tests for all API endpoints

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -6,3 +6,51 @@ test('connects to express server', async () => {
     expect(response.ok).toBe(true);
     expect(response.status).toBe(200);
 });
+
+// connect to login route
+test('connects to login route', async () => {
+    const response = await fetch('http://localhost:3000/login/');
+    expect(response.ok).toBe(true);
+    expect(response.status).toBe(200);
+});
+
+// connect to signup route
+test('connects to signup route', async () => {
+    const response = await fetch('http://localhost:3000/signup/');
+    expect(response.ok).toBe(true);
+    expect(response.status).toBe(200);
+});
+
+// connect to task GET route
+test('connects to task GET route', async () => {
+    const response = await fetch('http://localhost:3000/task/');
+    expect(response.ok).toBe(true);
+    expect(response.status).toBe(200);
+});
+
+// connect to task POST route
+test('connects to task POST route', async () => {
+    const response = await fetch('http://localhost:3000/task/', {
+        method: 'POST'
+    });
+    expect(response.ok).toBe(true);
+    expect(response.status).toBe(200);
+});
+
+// connect to task PUT route
+test('connects to task PUT route', async () => {
+    const response = await fetch('http://localhost:3000/task/', {
+        method: 'PUT'
+    });
+    expect(response.ok).toBe(true);
+    expect(response.status).toBe(200);
+});
+
+// connect to task DELETE route
+test('connects to task DELETE route', async () => {
+    const response = await fetch('http://localhost:3000/task/', {
+        method: 'DELETE'
+    });
+    expect(response.ok).toBe(true);
+    expect(response.status).toBe(200);
+});
