38a316689809cc3999911cf32d0fb1a9b9e9a0f7
Leithen <leithen113@gmail.com>
2022-03-22 16:23:19 -0600

    Created initial Task model

---
+++ models/Task.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Task extends Model {};
+
+Task.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'pending'
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    }, 
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'task'
+    }
+)
