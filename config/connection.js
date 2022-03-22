9a76a65aa1d4a497b68cd818a39588b5e8d430b5
Leithen <leithen113@gmail.com>
2022-03-22 14:32:45 -0600

    Created basic User model

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
+// instantiate sequelize with postgres database
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     dialect: 'postgres'
 });
