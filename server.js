9d079afac4914888fd0973f06e65f057de3e41d4
Leithen <leithen113@gmail.com>
2022-03-22 13:30:09 -0600

    Created basic express server

---
+++ server.js
@@ -0,0 +1,17 @@
+// import express package
+const express = require('express');
+// create an express server call 'app'
+const app = express();
+// define which port the express server will run on
+const PORT = process.env.PORT || 3000;
+// set up express server middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// sample GET route to ensure server works
+app.get('/', (req, res) => {
+    res.send('The Server Works!');
+});
+
+// start running the app on the port defined at the top of the file
+app.listen(PORT, () => { console.log(`App listening on port ${PORT}`) });
