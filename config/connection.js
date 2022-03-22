a1e8e2ea1adf2f2a7635708b6958d0840008cbbd
Leithen <leithen113@gmail.com>
2022-03-22 14:19:44 -0600

    Connected sequelize to express via postgres. Set up .env variable

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+// configure dotenv package to use environment variable
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    dialect: 'postgres'
+});
+
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // import express package
 const express = require('express');
+// import sequelize connection
+const sequelize = require('./config/connection');
 // create an express server call 'app'
 const app = express();
 // define which port the express server will run on
 const PORT = process.env.PORT || 3000;
+
 // set up express server middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,5 +16,8 @@ app.get('/', (req, res) => {
     res.send('The Server Works!');
 });
 
+
 // start running the app on the port defined at the top of the file
-app.listen(PORT, () => { console.log(`App listening on port ${PORT}`) });
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => { console.log(`App listening on port ${PORT}`) });
+})
